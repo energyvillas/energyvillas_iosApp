97ca487b44604c2f92d4380da5f26975ae1f6f6d
Γεώργιος Γράβος <george@Georgioss-MacBook-Air.local>
2013-03-20 09:05:25 +0200

    Initial Commit

---
+++ DP/DPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPAppDelegate.h
+//  DP
+//
+//  Created by Γεώργιος Γράβος on 3/20/13.
+//  Copyright (c) 2013 Γεώργιος Γράβος. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DP
+//
+//  Created by Γεώργιος Γράβος on 3/20/13.
+//  Copyright (c) 2013 Γεώργιος Γράβος. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
