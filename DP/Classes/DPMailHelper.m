d1fb23c6681197838020caebbb85550b37748f01
george <george.gravos@gmail.com>
2013-06-22 19:44:20 +0300

    social(mail) texts

--- DP/Classes/DPMailHelper.m
+++ DP/Classes/DPMailHelper.m
@@ -23,6 +23,7 @@
     NSString *device = IS_IPAD ? @"iPad" : @"iPhone";
     NSString *evurl = [CURRENT_LANG isEqualToString:@"el"] ? @"http://www.energeiakikatoikia.gr" : @"http://www.energyvillas.com";
     NSString *imgname = [DPAppHelper sharedInstance].imageUrl2Share;
+    NSString *imgdescr = [DPAppHelper sharedInstance].imageTitle2Share;
     UIImage *img = [[DPAppHelper sharedInstance] loadUIImageFromCache:imgname];
     CGSize sz = img.size;
     if (sz.width > 300 || sz.height > 300) {
@@ -36,8 +37,11 @@
         }
     }
     
-	NSString *mailBody = [NSString stringWithFormat:bodyfmt, device, imgname, imgname, sz.width, sz.height, evurl];
+	NSString *mailBody = [NSString stringWithFormat:bodyfmt, /*device, */imgname, imgdescr,
+                          imgname, imgname, imgdescr, sz.width, sz.height,
+                          evurl, evurl];
 	
+    NSLog(@"%@", mailBody);
 	MFMailComposeViewController *mailComposer = [[MFMailComposeViewController alloc] init];
 	[mailComposer setSubject:mailSubject];
 	[mailComposer setMessageBody:mailBody isHTML:TRUE];
